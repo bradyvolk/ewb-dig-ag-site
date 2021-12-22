fadb988210af10b45e83ca8c034a31134be31880
bradyvolk <39806319+bradyvolk@users.noreply.github.com>
2021-12-22 15:20:38 -0500

    Init repo

---
+++ ewb-dig-ag-site/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <span>Insert dig ag site here</span>
+    </div>
+  );
+}
+
+export default App;
