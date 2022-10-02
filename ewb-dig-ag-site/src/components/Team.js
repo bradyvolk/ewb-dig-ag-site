1a0fc38d9463888967114712ef28ec9406f96d8e
sc2448 <sc2448@cornell.ed>
2022-10-02 15:09:59 -0400

    small change

--- ewb-dig-ag-site/src/components/Team.js
+++ ewb-dig-ag-site/src/components/Team.js
@@ -30,6 +30,43 @@ function Team() {
           <Title> The Team </Title>
         </Overlay>
       </Container>
+      <OddSection>
+        <Row>
+          <Column>
+            <ContentContainer>
+              <PrimaryTitle> History and Background </PrimaryTitle>
+              <TextContainer>
+                Cornell Engineers Without Borders is a Cornell University
+                project team affiliated with the national Engineers Without
+                Borders Organization. The Digital Agriculture Team, led by
+                Jessica Henson, is a part of Cornell's EWB chapter.
+              </TextContainer>
+              <TextContainer>
+                The Digital Agriculture team is split into two subteams:
+                hardware and software. The hardware team focuses on the
+                construction and operation of the rover and drone, and the
+                software team focuses on the machine learning algorithms, this
+                website, and applications for interfacing with the hardware and
+                datasets.
+              </TextContainer>
+              <TextContainer>
+                The idea to use digital agriculture tools to identify northern
+                leaf blight disease in maize crop was first explored on campus
+                by undergraduate Nathan Zheng ('21), postdoctoral researcher
+                Ethan Stewart and Professor Michael Gore in the summer of 2019.
+                After a strong research proposal paper, the project received
+                grants and was adopted by Engineers Without Borders at Cornell
+                and which became known as the Digital Agriculture sub-team.
+              </TextContainer>
+            </ContentContainer>
+          </Column>
+          <Column>
+            <ContentContainer>
+              <Image src={nlb_photo}></Image>
+            </ContentContainer>
+          </Column>
+        </Row>
+      </OddSection>
     </div>
   );
 }
