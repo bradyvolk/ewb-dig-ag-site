df0e50063d39254c8df0b693fb27bec090144475
sc2448 <sc2448@cornell.ed>
2022-10-02 15:23:45 -0400

    fixing main with contact page

--- ewb-dig-ag-site/src/App.js
+++ ewb-dig-ag-site/src/App.js
@@ -3,6 +3,7 @@ import NavContent from "./components/NavContent";
 import Home from "./components/Home";
 import Overview from "./components/Overview";
 import Team from "./components/Team";
+import Contact from "./components/Contact";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import {} from "./components/shared/Section.styles";
