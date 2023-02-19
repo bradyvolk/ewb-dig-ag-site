81e2968bd98a6a3d585e9a5e9cf9b47bfa55efb8
Shivani Lal <shl94@cornell.edu>
2023-02-19 15:31:16 -0500

    Wording changes

--- ewb-dig-ag-site/src/components/Home.js
+++ ewb-dig-ag-site/src/components/Home.js
@@ -13,15 +13,15 @@ function Home() {
       <CardContainer>
         <Card className="text-center">
           <Card.Body>
-            <Card.Title>What we’re about:</Card.Title>
+            <Card.Title>Our Mission:</Card.Title>
             <Card.Text>
               Undetected outbreaks of disease in crops can be devastating for
               local economies in Sub-Saharan Africa. Northern Leaf Blight (NLB)
               is an example of one of these diseases that is caused by a
-              specific type of fungus. NLB spreads from plant to plant,
+              specific type of fungus. NLB spreads rapidly from plant to plant,
               rendering large portions of maize crops useless. To aid farmers,
               Engineers Without Borders at Cornell is currently building a drone
-              and a ground rover in order to assess large plots of crop fields
+              and ground rover in order to assess large plots of crop fields
               for NLB in real-time. The drone will detect infrared radiation to
               provide a large aerial assessment of overall field health, which
               will direct the rover to survey flagged sections of the field for
